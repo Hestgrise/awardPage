1ffb1299602cbcf73433bd61318247fe39bbe053
burrellp <pburrell05@gmail.com>
2016-07-11 23:52:53 -0700

    mysql connector syntax example

--- api.py
+++ api.py
@@ -12,9 +12,17 @@ import webapp2
 import jinja2
 from jinja2 import Environment, PackageLoader
 import os
-
+import mysql.connector
 import json
 
+dbName = 'lambda'
+dbUser = 'student'
+dbPass = 'default'
+dbHost = 'localhost'
+
+#Create database connection and cursor for queries
+cnx = mysql.connector.connect(user='root', password='password', host='localhost', database='ESDR2')
+cursor = cnx.cursor(buffered=True)
 
 class SignOnPage(webapp2.RequestHandler):
 	def get(self):
@@ -28,5 +36,38 @@ class SignOnPage(webapp2.RequestHandler):
 
 		out_obj = {'msg': 'Success, Your POST worked!'}
 		self.response.out.write(json.dumps(out_obj))
+
+class CheckLogin(webapp2.RequestHandler):
+	def post(self):
+		#Simple example of login validation to show mysql connector syntax
+		validLogin = False
+		#Assume the sign on page POSTS a username and password
+		username = self.request.get('username', default_value=None)
+		password = self.request.get('password', default_value=None)
+		#(1)Create the MySQL command, (2)Execute it
+		userQuery = ("SELECT password FROM users WHERE name = '"+username+"'")
+		cursor.execute(userQuery)
+		passwordInDB = cursor.fetchone()
+		#Check entered password against user's stored password
+		if passwordInDB == password:
+			#Redirect to landing page
+		else:
+			#Error message. Try again
+
+class CreateUserAccount(webapp2.RequestHandler):
+	def post(self):
+		username = self.request.get('username')
+		name = self.request.get('name')
+		password = self.request.get('password')
+		signature = "haven't figured this out yet"
+		instant = "use datetime.datetime.now()"
+
+		userDetails = (name, username, password, instant, signature)
+		userInsert = ("INSERT INTO users (name, email, password, dateCreated, signature) VALUES (%s, %s, %s, %s, %s)")
+		#Additional execute() call needed for insert/update/delete commands
+		cursor.execute(userInsert, userDetails)
+
+
+
 			
 			
--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import webapp2_static
 
 config = {'default-group' : 'base-data'}
 
+#Comment/uncomment hostName as needed for local/AWS testing
+hostName = 'localhost'
+#hostName = 'ec2-52-26-46-121.us-west-2.compute.amazonaws.com'
+portNum = '5432'
+
 app = webapp2.WSGIApplication([
 (r'/static/(.+)', webapp2_static.StaticFileHandler)
 ], debug=True, config=config)
@@ -10,7 +15,7 @@ app = webapp2.WSGIApplication([
 
 def main():
 	from paste import httpserver
-	httpserver.serve(app, host='ec2-52-26-46-121.us-west-2.compute.amazonaws.com', port='5423')
+	httpserver.serve(app, host=hostName, port=portNum)
 	
 app.router.add(webapp2.Route(r'/', 'api.SignOnPage'))
 app.router.add(webapp2.Route(r'/adapter/<id:[0-9]+><:/?>', 'api.AdapterVar'))
