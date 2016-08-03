00aa6d46a1f6e4503ef3a22977388cd062e56544
johnsan7 <johnsan7@oregonstate.edu>
2016-08-02 22:08:23 -0500

    Made award loader into a function so it can be called after award is added

--- static/script/dashboard.js
+++ static/script/dashboard.js
@@ -3,33 +3,45 @@ document.addEventListener('DOMContentLoaded', pageLoad);
 
 function pageLoad()
 {
-	var createReq = new XMLHttpRequest();
 
-	createReq.open('POST', '../recentAwards', true);
-	createReq.setRequestHeader('Content-Type', 'application/json');
-	createReq.addEventListener('load', function() {
-		if (createReq.status >= 200 && createReq.status < 400) {
-			var response = JSON.parse(createReq.responseText);
-			var list = document.getElementById("recentAwardsList")
-			var numAwards = response.names.length;
-			for (var i = 0; i < numAwards; i++) {
-				var li = document.createElement("li");
-				var name = String(response.names[i]);
-				var award = String(response.awards[i]);
-				var date = String(response.dates[i]);
-				var textNode = name + " was awarded " + award + " on " + date;
-				li.appendChild(document.createTextNode(textNode));
-				list.appendChild(li);
+
+	function showAwards(event)	{
+		
+		//First we remove any awards that are present
+		oldAwards = document.getElementById('recentAwardsList');
+		while(oldAwards.firstChild){
+			oldAwards.removeChild(oldAwards.firstChild);
+		}
+		var createReq = new XMLHttpRequest();
+
+		createReq.open('POST', '../recentAwards', true);
+		createReq.setRequestHeader('Content-Type', 'application/json');
+		createReq.addEventListener('load', function() {
+			if (createReq.status >= 200 && createReq.status < 400) {
+				var response = JSON.parse(createReq.responseText);
+				var list = document.getElementById("recentAwardsList")
+				var numAwards = response.names.length;
+				for (var i = 0; i < numAwards; i++) {
+					var li = document.createElement("li");
+					var name = String(response.names[i]);
+					var award = String(response.awards[i]);
+					var date = String(response.dates[i]);
+					var textNode = name + " was awarded " + award + " on " + date;
+					li.appendChild(document.createTextNode(textNode));
+					list.appendChild(li);
+				}
 			}
-		}
-		else {
-			document.getElementById('result').textContent = "Server is unreachable at this time";
-		}
-	});
+			else {
+				document.getElementById('result').textContent = "Server is unreachable at this time";
+			}
+		});
 
-	createReq.send();
-	//event.preventDefault();
-
+		createReq.send();
+		//event.preventDefault();
+	};
+	//We call the function to fill in the awards defined just above
+	showAwards();
+	
 	var submitButton = document.getElementById('certSubmit');
 	submitButton.onclick = function(event)
 	{
@@ -60,7 +72,7 @@ function pageLoad()
 				document.getElementById('result').textContent = "Server is unreachable at this time";
 			}
 			
-			
+			showAwards();
 			
 		});
 		
