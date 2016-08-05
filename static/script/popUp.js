2d19f9625a52f30ddf6502ff4c295fe280775dc0
Ubuntu <ubuntu@ip-172-31-41-151.us-west-2.compute.internal>
2016-08-05 02:30:12 +0000

    Modified error checking and added pop up error notification

--- static/script/popUp.js
+++ static/script/popUp.js
@@ -1,12 +1,16 @@
-// makes the filter menu visible
-function spawn() {
-	var form = document.getElementById("filterMenu");
+// global variable that stores the table data
+// modified everytime a new query runs
+var queryData = null;
+
+// makes the element visible
+function spawn(idVal) {
+	var form = document.getElementById(idVal);
 	form.style.display="block";
 }
 
-// makes the filter menu not visible
-function deSpawn() {
-	var form = document.getElementById("filterMenu");
+// makes the element not visible
+function deSpawn(idVal) {
+	var form = document.getElementById(idVal);
 	form.style.display="none";
 }
 
@@ -19,38 +23,34 @@ var url = '../filterData';
 
 function filterReq() {
 
-    // first close the filter menu
-    deSpawn();
-
     var data={}; // initialize dictionary
     var fTable = document.getElementById("filterDataTable"); // get the table
-    fTable.innerHTML="";
 
-    var header = fTable.createTHead();
-    var rowHeader = header.insertRow();
+    var headers = [];
 
     var fields = document.getElementsByClassName("fields");
     for (var i=0,y=0; i<fields.length;i++) {
         if (fields[i].checked) {
             // use a switch statement to determine the header content
+            // push the header names to the headers array
             switch (fields[i].value) {
                 case "type":
-                    rowHeader.insertCell(y).innerHTML="Award Type";
+                    headers.push("Award Type");
                     break;
                 case "awardee":
-                    rowHeader.insertCell(y).innerHTML="Awardee Name";
+                    headers.push("Awardee Name");
                     break;
                 case "email":
-                    rowHeader.insertCell(y).innerHTML="Awardee Email";
+                    headers.push("Awardee Email");
                     break;
                 case "dateAwarded":
-                    rowHeader.insertCell(y).innerHTML="Date Created";
+                    headers.push("Date Created");
                     break;
                 case "userId":
-                    rowHeader.insertCell(y).innerHTML="User ID";
+                    headers.push("User ID");
                     break;
                 case "userName":
-                    rowHeader.insertCell(y).innerHTML="User Full Name";
+                    headers.push("User Full Name");
                     break;
                 default:
                     console.log("Error printing out table header");
@@ -60,21 +60,38 @@ function filterReq() {
         }
     }
 
-    // if no fields are selected, then don't submit a HTTP POST request
+    // if y==0, then no headers were selected 
+    // show error in a notification and return
     if (y==0) {
-        // pop up to let user know to select options
+        spawn('notif');
         return;
     }
 
+    // get the filter values
+    data["filter"] = document.getElementById("filters").value;
+    data["filterVal"] = document.getElementById("filterVal").value;
+    
+
+    // if a filter option is provided but no filter value is provided, then alert the user and return
+    if (data["filter"] != "none" && data["filterVal"] == "") {
+        spawn('notif');
+        return;
+    }
+
+    // once this point is reached, the filter options are accepted
+    deSpawn('filterMenu'); // close the window
+
+    // clear the table and add in the headers
+    // header titles are stored in the headers array
+    fTable.innerHTML="";
+    var header = fTable.createTHead();
+    var rowHeader = header.insertRow();
+
+    for (var t=0; t<headers.length; t++) {
+        rowHeader.insertCell(t).innerHTML = headers[t];
+    }
+
     data["size"] = y;
-
-    // get filter option
-    data["filter"] = document.getElementById("filters").value;    
-    data["filterVal"] = document.getElementById("filterVal").value;    
-    console.log(data);
-
-    if (data["filterVal"] == "")
-        console.log("nada");
 
     var filterData = JSON.stringify(data); // make into JSON format
     // data is now in dictionary format - send to python api.py
@@ -91,8 +108,8 @@ function filterReq() {
     filterOptions.addEventListener('load', function() {
         if (filterOptions.status >= 200 && filterOptions.status < 400) {
             var response = JSON.parse(filterOptions.responseText);
-//            console.log(response.length);
-
+            
+            queryData = response;
             // display the results in the table
            var footer = fTable.createTFoot();
            var newRow, temp;
@@ -113,4 +130,10 @@ function filterReq() {
 
 }
 
+function exportCSV() {
 
+    if (queryData != null && queryData != "") {
+        console.log(queryData);
+    }
+
+}
